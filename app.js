2008c8769af18aa79d98c140e989b40f83415dcb
rockz0 <a8285443826@gmail.com>
2019-11-23 00:12:11 +0530

    upload one file

---
+++ app.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.uploadDir='./uploadedfile';
+    form.keepExtensions=true;
+    form.parse(req,(err,fields,files)=>{
+          if(err)
+          throw err;
+          res.write('ok');
+          res.end();
+    })
+ }
+   else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
