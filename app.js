f1a522472f257e97c84aec2bdbf59eb12e38dfe6
rockz0 <a8285443826@gmail.com>
2019-11-24 18:00:39 +0530

    size issue fixed, empty file isuue fixed

--- app.js
+++ app.js
@@ -7,20 +7,42 @@ app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', function (req, res){
+app.post('/',function (req, res){
     var form = new formidable.IncomingForm();
+    //For multiples file 
+    form.multiples=true;
 
+    //for max file size in bytes
+    form.maxFileSize=20*1024*1024;
+
+    //parse the req
     form.parse(req);
+    
+    form.on('error',(err)=>{
+      res.send('error');
+    });
 
     form.on('fileBegin', function (name, file){
+      console.log(form.bytesExpected);
+      if(form.bytesExpected<1024)
+      {
+        res.sendFile(__dirname + '/index.html');
+      }
+      else
+      {
+        //for the path of uplaoding file, can also use form.uploadDir
         file.path = __dirname + '/uploadedfile/' + file.name;
+      }
+      
     });
 
     form.on('file', function (name, file){
+        if(file.name)
         console.log('Uploaded ' + file.name);
+        res.sendFile(__dirname + '/index.html');
     });
 
-    res.sendFile(__dirname + '/index.html');
+    
 });
 
 app.listen(3000,(err)=>{
